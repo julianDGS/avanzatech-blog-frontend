5d82add6a760e40d02c6255f393104afb4ed5234
Julian Garzon <jdjuliangs@gmail.com>
2024-05-22 10:45:35 -0500

    logout action

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -3,6 +3,8 @@ import { Component, signal } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -15,8 +17,25 @@ export class TopbarComponent {
   
   dark_mode = signal<boolean>(true);
 
+  constructor(
+    private authSV: AuthService,
+    private router: Router
+  ){}
+
   toggleTheme(){
     this.dark_mode.update(prev => !prev)
+  }
+
+  onLogout(){
+    this.authSV.logout().subscribe({
+      next: (resp) => {
+        console.log(resp);
+        this.router.navigate(['auth'])
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthRequest, LoginResponse } from '../../models/auth/login.model';
 import { environment } from '../../../environments/environment'
+import { MessageModel } from '../../models/util/message.model';
+
+import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +13,23 @@ import { environment } from '../../../environments/environment'
 export class AuthService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private snackBar: MatSnackBar
   ) { }
 
   login(request: AuthRequest){
     return this.http.post<LoginResponse>(`${environment.api_url}/user/login/`, request, {withCredentials: true});
+  }
+
+  logout(){
+    return this.http.get<MessageModel>(`${environment.api_url}/user/logout/`, {withCredentials: true}).pipe(
+      catchError(err => {
+        this.snackBar.open(err, 'Close', {
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+        return throwError(() => err);
+      })
+    );
   }
 }
