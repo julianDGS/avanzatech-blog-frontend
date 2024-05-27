7e227a3b59c02e2c0cbad7bfcfa6f490b5f04f51
Julian Garzon <jdjuliangs@gmail.com>
2024-05-27 15:57:54 -0500

    Like service created

---
+++ src/app/models/like/like.model.ts
@@ -0,0 +1,17 @@
+import { Post } from "../post/post.model";
+import { User } from "../user/user.model";
+
+export interface PaginatedLike {
+    next:         string;
+    previous:     string;
+    total_count:  number;
+    current_page: number;
+    total_pages:  number;
+    results:      Like[];
+}
+
+export interface Like {
+    id:   number;
+    post: Pick<Post, 'id' | 'title'>;
+    user: Omit<User, 'name' | 'lastname' | 'team'>;
+}
---
+++ src/app/services/like/like.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../util/http.service';
+import { PaginatedLike } from '../../models/like/like.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LikeService {
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  getLikes(pageNumber: string, post_id: string, user_id=''){
+    const url = `like/?page=${pageNumber ? pageNumber : ''}&post=${post_id}&user=${user_id}`
+    return this.http.get<PaginatedLike>(url, false)
+  }
+}
