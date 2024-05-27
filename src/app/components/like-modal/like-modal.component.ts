d330e198fd08da2c551ff574ce28e35f8979868f
Julian Garzon <jdjuliangs@gmail.com>
2024-05-27 15:59:19 -0500

    Like modal created

--- src/app/components/like-modal/like-modal.component.ts
+++ src/app/components/like-modal/like-modal.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PaginatedLike } from '../../models/like/like.model';
+import { CommonModule } from '@angular/common';
+import { PaginatorComponent } from '../paginator/paginator.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-like-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PaginatorComponent, MatCardModule],
   templateUrl: './like-modal.component.html',
   styleUrl: './like-modal.component.scss'
 })
 export class LikeModalComponent {
 
+  itemsPerPage = 20;
+
+  @Input({required: true}) paginatedLike?: PaginatedLike;
+  @Output() pageChanged = new EventEmitter<string>();
+
+  changePage(page: number){
+    const pageStr = String(page)
+    this.pageChanged.emit(pageStr)
+  }
 }
