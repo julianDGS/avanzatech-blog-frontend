f080e2733120e9d693d9cc92c1dd24d052228c84
Julian Garzon <jdjuliangs@gmail.com>
2024-05-31 12:22:10 -0500

    Form configuration for post create

--- src/app/pages/principal/posts/post-create/post-create.component.ts
+++ src/app/pages/principal/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, ViewChild, signal } from '@angular/core';
 import { PostService } from '../../../../services/post/post.service';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PermissionService } from '../../../../services/post/permission.service';
 import { PermissionResponse } from '../../../../models/post/permission.model';
 import { switchMap } from 'rxjs';
@@ -11,12 +11,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import { PostRequest } from '../../../../models/post/post-request.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    
     MatCardModule, 
     MatFormFieldModule,
     MatInputModule,
@@ -33,10 +35,13 @@ export class PostCreateComponent implements OnInit{
   permissions = signal<PermissionResponse[]>([]);
   categories = signal<PermissionResponse[]>([]); 
 
+  @ViewChild(FormGroupDirective) formDir!: FormGroupDirective;
+
   constructor(
     private postSV: PostService,
     private permissionSV: PermissionService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ){
     this.buildForm()
   }
@@ -44,38 +49,86 @@ export class PostCreateComponent implements OnInit{
   ngOnInit(): void {
     this.permissionSV.getCategories().pipe(
       switchMap(resp => {
-        this.categories.set(resp.sort((a, b) => b.id - a.id))
-        this.loadCategories()
+        this.categories.set(resp) //.set(resp.sort((a, b) => b.id - a.id))
         return this.permissionSV.getPermissions()
       })
     ).subscribe(resp => {
       this.permissions.set(resp)
+      const ids = this.changeNames();
+      this.loadCategories(ids);
     })
   }
 
-  private loadCategories(){
+  private changeNames(){
+    let read = 0;
+    let edit = 0;
+    for(let permission of this.permissions()){
+      if(permission.name === 'read'){
+        read = permission.id;
+        permission.name = 'read only'
+      } else if (permission.name === 'edit'){
+        edit = permission.id;
+        permission.name = 'read & write'
+      }
+    }
+    return {read, edit}
+  }
+
+  private loadCategories(ids: {read: number, edit: number}){
     for (let category of this.categories()){
-      this.addPermission(category.id)
+      let permId;
+      switch(category.name){
+        case 'author':
+          permId = ids.edit;
+          category.name = 'Owner'
+          break;
+        case 'team':
+          permId = ids.edit;
+          category.name = 'Team'
+          break;
+        case 'auth':
+          permId = ids.read;
+          category.name = 'Authenticated'
+          break;
+        case 'public':
+          permId = ids.read;
+          category.name = 'Public'
+          break;
+        }
+      this.addPermission(category.id, permId)
     }
   }
 
   onSubmit(){
     if(!this.postForm.pristine){
-      const request: PostRequest = this.postForm.value 
-      console.log(this.postForm.value);
+      this.postForm.enable()
+      const request: PostRequest = this.postForm.value
       console.log(request);
+      this.disableCategories()
+    }
+  }
+
+  onCancel(){
+    this.resetForm()
+    this.router.navigate(['/'])
+  }
+
+  private resetForm(){
+    this.formDir.resetForm();
+    this.disableCategories();
+  }
+
+  private disableCategories(){
+    for (let i = 0; i < this.permissionsForm.controls.length; i++) {
+      this.permissionsForm.controls[i].get('category_id')?.disable();
     }
   }
 
   buildForm(){
     this.postForm = this.fb.group({
-      title: [null, []],
-      content: [null, []],
-      // author: [null, []],
-      // team: [null, []],
-      // authenticate: [null, []],
-      // public: [null, []],
-      permissions: this.fb.array([])
+      title: [null, [Validators.required, Validators.maxLength(100)]],
+      content: [null, [Validators.required, Validators.minLength(5)]],
+      permissions: this.fb.array([], {validators: [Validators.required, Validators.minLength(4)]})
     })
   }
 
@@ -86,8 +139,8 @@ export class PostCreateComponent implements OnInit{
   private addPermission(categoryId: number, permissionId?: number){
     this.permissionsForm.push(
       this.fb.group({
-        category_id: [ {value: categoryId, disabled: true} , []],
-        permission_id: [ permissionId ? permissionId : null , []]
+        category_id: [ {value: categoryId, disabled: true} , [Validators.required]],
+        permission_id: [ permissionId ? permissionId : null , [Validators.required]]
       })
     )
   }
