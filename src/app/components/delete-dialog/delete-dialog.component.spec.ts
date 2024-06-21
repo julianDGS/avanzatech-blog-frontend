2cf19a9d9953a7390d782b9f9bd4171dcc7bccbe
Julian Garzon <jdjuliangs@gmail.com>
2024-06-21 09:15:04 -0500

    Delete Dialog tests

--- src/app/components/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/components/delete-dialog/delete-dialog.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteDialogComponent } from './delete-dialog.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
+  const mockDialogData = { title: 'Delete Item', item: 'Item 1' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteDialogComponent]
+      imports: [DeleteDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close'])},
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData }
+      ]
     })
     .compileComponents();
     
@@ -19,5 +25,10 @@ describe('DeleteDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct data', () => {
+    expect(component.data.title).toBe(mockDialogData.title);
+    expect(component.data.item).toBe(mockDialogData.item);
   });
 });
